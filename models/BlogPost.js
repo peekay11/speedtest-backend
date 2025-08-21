e68d51f2a7fd202f989076bf833f17bd02a4e79e
Paseka Mabitsela <pasekamabitsela22@gmail.com>
2025-08-21 21:22:02 +0200

    Convert BlogPost.js to ES module syntax

---
+++ models/BlogPost.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const BlogPostSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+  author: { type: String, default: 'Anonymous' },
+  tags: [String],
+  date: { type: String, required: true },
+  readTime: { type: Number, required: true }
+});
+
+export default mongoose.model('BlogPost', BlogPostSchema);
